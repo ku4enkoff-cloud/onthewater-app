a3e2f3de89a904052a4573745c54248598494ec7
Ku4 <ku4enkoff@gmail.com>
2026-04-18 19:29:03 +0300

    Update BoatDetailPage to include useRef for improved functionality

--- site/src/pages/BoatDetailPage.jsx
+++ site/src/pages/BoatDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { boatDetailPath, boatUrlSegment, parseBoatUrlParam } from '../boatUrl'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import './boatDetail.css'
